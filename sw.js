ed9136172c4e84162f02919d758e3bd2ca622bc0
abdullahbutt <abdullah.butt@msn.com>
2026-06-27 06:53:38 +0200

    quick fixes

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v3
-const CACHE_NAME = 'deutsch-lernen-v4.0.9';
+const CACHE_NAME = 'deutsch-lernen-v4.0.10';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
