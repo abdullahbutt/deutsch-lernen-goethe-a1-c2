d65735238d4ca1328f29a4a8c2e8e9ac3cb6c15a
abdullahbutt <abdullah.butt@msn.com>
2026-07-04 19:37:24 +0200

    Update wortschatz filters and search bar improve

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v4
-const CACHE_NAME = 'deutsch-lernen-v4.0.7b';
+const CACHE_NAME = 'deutsch-lernen-v4.0.7c';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
