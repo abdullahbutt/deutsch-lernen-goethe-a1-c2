15e9d65c8be205854b673b8433534419ba8fddc7
abdullahbutt <abdullah.butt@msn.com>
2026-07-04 05:14:32 +0200

    Batch 3 verbs conjugations

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v3
-const CACHE_NAME = 'deutsch-lernen-v4.0.23a';
+const CACHE_NAME = 'deutsch-lernen-v4.0.23b';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
