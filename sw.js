1cc32f15c3dbb1dec35505d6545e8ae29389a7ec
abdullahbutt <abdullah.butt@msn.com>
2026-07-05 13:51:50 +0200

    Update A2 Wortliste fix all issues

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v4
-const CACHE_NAME = 'deutsch-lernen-v4.0.7o';
+const CACHE_NAME = 'deutsch-lernen-v4.0.7p';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
