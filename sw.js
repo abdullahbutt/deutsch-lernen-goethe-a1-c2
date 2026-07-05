0f6b1dbafc4f4eb1644f8a5d6ca8d9044945f454
abdullahbutt <abdullah.butt@msn.com>
2026-07-05 16:27:29 +0200

    Remove 3 duplicates

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v4
-const CACHE_NAME = 'deutsch-lernen-v4.0.7u';
+const CACHE_NAME = 'deutsch-lernen-v4.0.7v';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
