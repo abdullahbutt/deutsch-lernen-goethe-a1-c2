55bb99fe0da7aaebdd0c5d4645d87814cde3f651
abdullahbutt <abdullah.butt@msn.com>
2026-07-09 13:11:41 +0200

    udpate homepage words count again

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v4
-const CACHE_NAME = 'deutsch-lernen-v4.0.7.1d';
+const CACHE_NAME = 'deutsch-lernen-v4.0.7.1da';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
