d0ec002cb9af515d38dd9426c89eefb4032fb84d
abdullahbutt <abdullah.butt@msn.com>
2026-07-12 16:34:40 +0200

    Update app icons

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Deutsch Learning Hub - Service Worker v4
-const CACHE_NAME = 'deutsch-lernen-v4.0.7.1dk';
+const CACHE_NAME = 'deutsch-lernen-v4.0.7.1dl';
 const BASE = '/deutsch-lernen-goethe-a1-c2/';
 
 // Static assets that rarely change (cache-first)
